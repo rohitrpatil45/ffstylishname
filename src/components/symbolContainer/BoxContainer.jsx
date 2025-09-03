95e95eb4229abf9a7fd01b4136e64642efe72c93
Rohit Patil <rohitrajupatil12@gmail.com>
2025-09-03 18:40:31 +0530

    selected icon is now able toprint

--- src/components/symbolContainer/BoxContainer.jsx
+++ src/components/symbolContainer/BoxContainer.jsx
@@ -4,12 +4,16 @@ import { symbols, emojis } from "../data/datafile";
 
 
 function BoxContainer({heading , elements}) {
+ const wordprinting = (word, index) =>{
+console.log(word, index)
+ }
+
   return (
     <div className='main-container-symols'>
         <p className='box-heading'>{heading}</p>
         <div className='boxx-container'>
           {elements.map((element , index) =>(
-            <span key={index} className='element'>{element}</span>
+            <span onClick={() => {wordprinting(element,index)}} key={index} className='element'>{element}</span>
           ))}
 
         </div>
